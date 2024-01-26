d0d3e5312320bc909ca6ac0d72f81ceb8ed7f1b0
sparshsmith <sparshsmith@outlook.com>
2024-01-26 14:00:03 +0530

    state design pattern

---
+++ StatePattern/src/Main.java
@@ -0,0 +1,11 @@
+import ui.Player;
+import ui.UI;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
+    }
+}
---
+++ StatePattern/src/state/State.java
@@ -0,0 +1,16 @@
+package state;
+
+import ui.Player;
+
+public abstract class State {
+    Player player;
+
+    State(Player player){
+        this.player = player;
+    }
+
+    public abstract String onLock();
+    public abstract String onPlay();
+    public abstract String onNext();
+    public abstract String onPrevious();
+}
